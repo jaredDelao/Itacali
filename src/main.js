a3d6a1ebc0955a7c4308bd4207fe934083e5e337
jaredDelao <jaredhm07@gmail.com>
2023-01-14 15:36:19 -0600

    re

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import App from "./App.vue";
 import SwiperMain from "./components/SwiperMain.vue";
 import Soon from "./components/Soon.vue";
@@ -20,7 +20,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
